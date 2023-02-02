f72c84bc5de2ee922848c7a53e5e679916e06462
ChoiHyunSuk93 <121939797+ChoiHyunSuk93@users.noreply.github.com>
2023-02-02 17:33:00 +0900

    멀티캠퍼스 백엔드스쿨 9회차

---
+++ day04/out.js
@@ -0,0 +1,16 @@
+function 빨래하다() {
+    console.log("1. 먼지를 턴다.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 빤다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+function 청소하다() {
+    console.log("1. 짐을 정리한다.")
+    console.log("2. 창문을 연다.")
+    console.log("3. 먼지를 턴다.")
+    console.log("4. 청소기를 돌린다.")
+    console.log("5. 창문을 닫는다.")
+}
---
+++ day04/out2.js
@@ -0,0 +1,15 @@
+function 여행하다() {
+    console.log("1. 여행지를 정한다.")
+    console.log("2. 여행 계획을 짠다.")
+    console.log("3. 숙소를 예매한다.")
+    console.log("4. 교통편을 예매한다.")
+    console.log("5. 짐을 챙긴다.")
+    console.log("6. 출발한다.")
+    console.log("7. 즐긴다.")
+}
+function 쉬다() {
+    console.log("1. 짐을 푼다.")
+    console.log("2. 씻는다.")
+    console.log("3. 편한 옷으로 갈아입는다.")
+    console.log("4. 휴식을 한다.")    
+}
